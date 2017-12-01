8d6f2764586d902d793ff26ec733aed82086daf7
huangwb <huangwb@sunrizetech.com>
2017-12-01 17:56:43 +0800

    delete common

--- open-core/open-core-auth/src/main/java/com/kawhii/open/core/auth/config/OAuth2ServerConfig.java
+++ open-core/open-core-auth/src/main/java/com/kawhii/open/core/auth/config/OAuth2ServerConfig.java
@@ -46,8 +46,6 @@ public class OAuth2ServerConfig {
         @Override
         public void configure(HttpSecurity http) throws Exception {
             http
-                    // Since we want the protected resources to be accessible in the UI as well we need
-                    // session creation to be allowed (it's disabled by default in 2.0.6)
                     .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
                     .and()
                     .requestMatchers().antMatchers("/photos/**", "/oauth/users/**", "/oauth/clients/**", "/me")
--- open-core/open-core-auth/src/main/java/com/kawhii/open/core/auth/config/SecurityConfig.java
+++ open-core/open-core-auth/src/main/java/com/kawhii/open/core/auth/config/SecurityConfig.java
@@ -14,8 +14,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
  * 安全配合
