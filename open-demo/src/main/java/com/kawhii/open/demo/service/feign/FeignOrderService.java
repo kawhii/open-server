274d74409165683b7d4feb6c4fa333160dc81e70
黄文彬 <huangwb@sunrizetech.com>
2018-01-19 19:31:54 +0800

    删除多余代码

--- open-demo/src/main/java/com/kawhii/open/demo/service/feign/FeignOrderService.java
+++ open-demo/src/main/java/com/kawhii/open/demo/service/feign/FeignOrderService.java
@@ -4,13 +4,8 @@
 
 package com.kawhii.open.demo.service.feign;
 
-import com.kawhii.open.demo.domain.Order;
 import com.kawhii.open.demo.service.OrderService;
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-import java.util.List;
 
 /**
  * @author Carl
@@ -19,12 +14,4 @@ import java.util.List;
  */
 @FeignClient("DEMO")
 public interface FeignOrderService extends OrderService {
-    //获取所有订单
-    @GetMapping(value = "/eureka-orders/fetch")
-    List<Order> findAll();
-
-
-    //获取id
-    @GetMapping(value = "/eureka-orders/{id}")
-    Order loadByItemCode(@PathVariable("id") String id);
 }
